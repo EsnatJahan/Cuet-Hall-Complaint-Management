46019ee1b52a9fe07cdbb678087ad791ddaadbcb
EsnatJahan <145742386+EsnatJahan@users.noreply.github.com>
2025-01-10 00:15:22 +0600

    Add Component folder

---
+++ frontend/src/Components/Header.tsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <div>
+            
+            <nav>
+                {
+                   /* <h1 className="text-2xl text-clr font-serif "  > Hello </h1> */
+                   <h1>Cuet Hall Complaint Management</h1>
+                }
+            </nav>
+            {/* <img src="src\assets\Love.jpg" alt="" height="300px" width="300px" className ="container - flex border-black"/> */}
+        </div>
+    )
+}
+
+export default Header
