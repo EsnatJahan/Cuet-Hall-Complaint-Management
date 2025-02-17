8c12fa0bb6181215a95294299be8bc862cb87acf
Trayee Das <trayeeddas49@gmail.com>
2025-02-17 19:02:51 +0600

    Profile

--- frontend/src/Components/Profile.tsx
+++ frontend/src/Components/Profile.tsx
@@ -1,28 +1,28 @@
-import React from "react";
-
-function ProfilePage() {
-  return (
+import React from 'react'
+function ProfilePage(){
+  return(
     <div className="max-w-md mx-auto mt-10 p-6 bg-white shadow-lg rounded-lg">
-      <h1 className="text-2xl font-bold mb-4 text-center">Profile</h1>
-      <table className="w-full border-collapse border border-gray-300">
-        <img src="man.png" alt="" />
-        <tbody>
-          {[
-            ["Name", "Esnat Jahan"],
-            ["Email", "ena@gmail.com"],
-            ["Hall", "CUET Tapshi Rabeya Hall"],
-            ["Room No", "220"],
-            ["Phone Number", "0147788"],
-          ].map(([key, value]) => (
-            <tr key={key} className="border-b border-gray-200">
-              <td className="px-4 py-2 font-semibold text-gray-700 bg-gray-100">{key}</td>
-              <td className="px-4 py-2 text-gray-800">{value}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <h1 className="text-2xl font-bold mb-4 text-center">Profile</h1>
+    <table className="w-full border-collapse border border-gray-300">
+      <img src="man.png" alt="" />
+      <tbody>
+        {[
+          ["Name", "Esnat Jahan"],
+          ["Email", "ena@gmail.com"],
+          ["Hall", "CUET Tapshi Rabeya Hall"],
+          ["Room No", "220"],
+          ["Phone Number", "0147788"]
+        ].map(([key, value]) => (
+          <tr key={key} className="border-b border-gray-200">
+            <td className="px-4 py-2 font-semibold text-gray-700 bg-gray-100">{key}</td>
+            <td className="px-4 py-2 text-gray-800">{value}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
   );
 }
+
 
 export default ProfilePage;
